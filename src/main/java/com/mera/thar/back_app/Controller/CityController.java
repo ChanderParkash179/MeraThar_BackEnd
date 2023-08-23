e877893086c58ced239fc7f9b9306a3a3beec34e
Chander Parkash <chanderparkash179@gmail.com>
2023-08-23 19:17:48 +0500

    added loggers in city controller

--- src/main/java/com/mera/thar/back_app/Controller/CityController.java
+++ src/main/java/com/mera/thar/back_app/Controller/CityController.java
@@ -1,6 +1,7 @@
 package com.mera.thar.back_app.Controller;
 
 import com.mera.thar.back_app.Service.CityService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.*;
 
 @RequestMapping("api/mera_thar/city/")
@@ -16,11 +19,14 @@ import java.util.*;
 @RequiredArgsConstructor
 public class CityController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CityController.class);
+
     private final CityService cityService;
 
     @PostMapping("get/id")
     private ResponseEntity<?> findById(@RequestBody Map<String, Object> request) {
         try {
+            logger.info("in CityController.findById() : {}");
             return new ResponseEntity<>(this.cityService.getById(request), HttpStatus.OK);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -30,6 +36,7 @@ public class CityController {
     @PostMapping("get/name")
     private ResponseEntity<?> findByName(@RequestBody Map<String, Object> request) {
         try {
+            logger.info("in CityController.findByName() : {}");
             return new ResponseEntity<>(this.cityService.getByName(request), HttpStatus.OK);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -39,6 +46,7 @@ public class CityController {
     @PostMapping("get/list")
     private ResponseEntity<?> findCities() {
         try {
+            logger.info("in CityController.findCities() : {}");
             return new ResponseEntity<>(this.cityService.getAll(), HttpStatus.OK);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -48,6 +56,7 @@ public class CityController {
     @PostMapping("save")
     private ResponseEntity<?> addCity(@RequestBody Map<String, Object> request) {
         try {
+            logger.info("in CityController.addCity() : {}");
             return new ResponseEntity<>(this.cityService.save(request), HttpStatus.CREATED);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -57,15 +66,17 @@ public class CityController {
     @PostMapping("update")
     private ResponseEntity<?> updateCity(@RequestBody Map<String, Object> request) {
         try {
+            logger.info("in CityController.updateCity() : {}");
             return new ResponseEntity<>(this.cityService.update(request), HttpStatus.CREATED);
         } catch (Exception ex) {
-            return new ResponseEntity<>( HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("delete")
     private ResponseEntity<?> deleteCity(@RequestBody Map<String, Object> request) {
         try {
+            logger.info("in CityController.deleteCity() : {}");
             return new ResponseEntity<>(this.cityService.delete(request), HttpStatus.OK);
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);

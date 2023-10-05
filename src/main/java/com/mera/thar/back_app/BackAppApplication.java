e408a6e3489c158ff51a06e0c3430c3f7d3be00b
Chander Parkash <chanderparkash179@gmail.com>
2023-10-05 19:00:32 +0500

    cors config

--- src/main/java/com/mera/thar/back_app/BackAppApplication.java
+++ src/main/java/com/mera/thar/back_app/BackAppApplication.java
@@ -2,6 +2,9 @@ package com.mera.thar.back_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackAppApplication {
@@ -10,4 +13,12 @@ public class BackAppApplication {
         SpringApplication.run(BackAppApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
